362e5babb2fcc48e8ba65765fbc23c27175b46b5
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2025-01-14 13:36:56 +0100

    chore(lau-444): fix typo

--- src/domain/token/InitTokenOptions.ts
+++ src/domain/token/InitTokenOptions.ts
@@ -7,5 +7,5 @@ export interface InitTokenOptions {
   tokenAddress: string;
   moonshot: Moonshot;
   curveType?: CurveType;
-  provider?: ethers.JsonRpcProvider; // @depricated
+  provider?: ethers.JsonRpcProvider; // @deprecated
 }
